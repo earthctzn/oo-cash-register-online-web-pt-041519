05d877e54506c9f0e7668630eb449b5ada9c3ee8
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 23:25:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,15 +4,15 @@ class CashRegister
   attr_accessor :items, :total
 
 
-  def initialize(total = 0, emp_disc = 0)
+  def initialize(total = 0, emp_disc.to_f)
     @total = total
     @emp_disc = emp_disc
-    @shopping_cart = {:names => @items = []}
+    @shopping_cart = [ @items = []]
     apply_discount(emp_disc)
   end
   
   def apply_discount(num = 0)
-   @total - @total * num.to_f / 100
+   grand_total = @total - @total * num.to_f / 100
     return "After the discount, the total comes to $#{@total}"
   end 
   
