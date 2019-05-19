6232cc04c81ce32b64f10729efd4971d94df7401
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 18:56:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,18 +1,18 @@
 class CashRegister
   
-  attr_accessor :items
-  @@basket = {
+  attr_accessor :items :total
+  @@basket = {}
 
   def initialize(total = 0.0, emp_disc = 0)
     @total = total
-    @emp_disc = emp_disc
+    apply_discount(emp_disc)
   end
   
   # def add_item(item, unit_price = 0.0)
   
   # end
   
-  # def apply_discount()
+  def apply_discount()
   
   # end
   
