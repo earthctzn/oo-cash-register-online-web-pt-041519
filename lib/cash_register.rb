1f6254bf843f2dceb29ca57f22155c55f1e04cc9
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 22:15:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
   
   def add_item(product_name, unit_price = 0.0, qty = 1)
     if qty > 1
-      @items << product_name
+      @items << product_name * qty
       @total += unit_price * qty
     else
       @total += unit_price
