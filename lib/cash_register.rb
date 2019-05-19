5f2d816eb59e19e847c375540b18682cd72c7fd0
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 22:56:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def apply_discount(num = 0)
-    grand_total = @total - @total * num / 100
+    grand_total = @total - @total * num.to_f / 100
     binding.pry
     @total = grand_total
     return "After the discount, the total comes to $#{@total}"
