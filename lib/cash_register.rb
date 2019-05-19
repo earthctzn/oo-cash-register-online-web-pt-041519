400083142109037ae816fa60d8c1e9751a3b8561
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 17:27:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
 
+  def initialize(total = 0.0, emp_disc = 0)
+    @total = total
+    @emp_disc = apply_discount
+  end
+  
+  def apply_discount()
+  
+  end
+  
+  def void_last_transaction()
+  
+  end
+
+
+
+
+
+
+
+
+
+
+end
