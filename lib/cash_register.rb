9a1401339d277c03650e1a1684f07aa7a0f29d6f
Caleb Aviles <earthctzn1@gmail.com>
2019-05-20 01:01:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,16 +23,24 @@ class CashRegister
     @transactions << unit_price
   end
   
+  # def apply_discount
+  #   if @emp_disc != nil
+  #     grand_total = @emp_disc/100.to_f
+  #     @total = @total - @total * grand_total
+  #     "After the discount, the total comes to $#{@total.to_i}."
+  #   else
+  #     "There is no discount to apply."
+  #   end 
+  # end 
   def apply_discount
-    if @emp_disc != nil
-      grand_total = @emp_disc/100.to_f
-      @total = @total - @total * grand_total
-       "After the discount, the total comes to $#{@total.to_i}."
+    if @discount != nil
+      deduct_percent = @discount/100.to_f
+      @total = @total - @total * deduct_percent
+      "After the discount, the total comes to $#{@total.to_i}."
     else
-       "There is no discount to apply."
-    end 
-  end 
-  
+      "There is no discount to apply."
+    end
+  end  
   def discount
    @emp_disc
   end
