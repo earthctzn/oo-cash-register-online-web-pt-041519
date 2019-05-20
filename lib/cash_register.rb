35c081e3fcd517bc8f346a62280ca97d5dbf551a
Caleb Aviles <earthctzn1@gmail.com>
2019-05-20 00:55:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
   attr_accessor :items, :total, :items, :transactions
 
 
-  def initialize(emp_disc=nil)
+  def initialize(emp_disc = nil)
     total = 0
     @total = total
     @emp_disc = emp_disc.to_f
