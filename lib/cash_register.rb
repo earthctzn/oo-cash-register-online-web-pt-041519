0167154f4c5e872e5b57cc2252d9b623ce232be4
Caleb Aviles <earthctzn1@gmail.com>
2019-05-20 01:14:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,9 +25,11 @@ class CashRegister
   
   def apply_discount
      if @emp_disc != nil
-       "There is no discount to apply."
+       grand_total = @emp_disc/100.to_f
+       @total = @total - @total * grand_total
+       "After the discount, the total comes to $#{@total.to_i}."
      else
-       
+       "There is no discount to apply."
     end 
    end 
  
