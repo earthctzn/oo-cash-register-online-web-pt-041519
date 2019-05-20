a2fd304fca17fff088c794841de6377c29e4b2cb
Caleb Aviles <earthctzn1@gmail.com>
2019-05-20 01:04:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
   def initialize(emp_disc = nil)
     total = 0
     @total = total
-    @emp_disc = emp_disc.to_f
+    @emp_disc = emp_disc
     @items = []
     @transactions = []
     apply_discount
@@ -40,7 +40,8 @@ class CashRegister
     else
       "There is no discount to apply."
     end
-  end  
+  end 
+  
   def discount
    @emp_disc
   end
