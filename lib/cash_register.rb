ad4cddd698e8092e111e4a2bc875cd7da68e298b
Caleb Aviles <earthctzn1@gmail.com>
2019-05-20 01:53:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -30,7 +30,16 @@ class CashRegister
        "There is no discount to apply."
     end
         
-  end 
+  end
+  # def apply_discount
+  #   if @discount != nil
+  #     deduct_percent = @discount/100.to_f
+  #     @total = @total - @total * deduct_percent
+  #     "After the discount, the total comes to $#{@total.to_i}."
+  #   else
+  #     "There is no discount to apply."
+  #   end
+  # end
   
   def discount
    @discount
